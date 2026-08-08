15c8bb606eef23a778eba13610fc52b8e93738cb
James Carman <jcarman@callibrity.com>
2026-08-08 15:25:27 -0400

    build: add Maven skeleton for nessy-core and nessy-testing

---
+++ nessy-core/src/test/java/com/callibrity/nessy/BuildSmokeTest.java
@@ -0,0 +1,13 @@
+package com.callibrity.nessy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class BuildSmokeTest {
+
+    @Test
+    void runsOnJava25OrLater() {
+        assertThat(Runtime.version().feature()).isGreaterThanOrEqualTo(25);
+    }
+}
